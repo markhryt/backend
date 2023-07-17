7a4953ac014a23afd7079474a1215eed4763f01c
Mark Hrytchak <markhrytchak@Fundraises-Air.localdomain>
2023-07-17 11:02:14 -0400

    Files structure changed
    :

---
+++ database/database.js
@@ -0,0 +1,11 @@
+const Sequelize = require('sequelize');
+require('dotenv').config();
+
+const urlParts = process.env.PG_CONNECTION_STRING.split(':');
+const dialect = urlParts[0].replace(/.*(?=postgres)/, 'postgres');
+const host = urlParts[2].replace(/\/\/|\/.*/g, '');
+
+module.exports = new Sequelize(process.env.PG_CONNECTION_STRING, {
+  dialect,
+  host,
+});
