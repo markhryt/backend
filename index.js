693cd45b8dcd072cf5e51b22731d16bef30aeeef
Mark Hrytchak <markhrytchak@Fundraises-Air.localdomain>
2023-07-18 14:38:25 -0400

    api-side cookies seted

--- index.js
+++ index.js
@@ -134,6 +134,7 @@ app.use(express.json());
   app.post('/login',passport.authenticate('local'),
   function(req, res) {
     res.clearCookie('sessionId');
+    res.cookie('sessionId', req.sessionID, {domain: "https://markse-commerce.netlify.app/"});
     res.json({sessionId: req.sessionID});
   });
   
