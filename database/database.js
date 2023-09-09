1d70396a6f3591b6d8ee42914077f5f8c7c05b1c
markhryt <hrytchakmark@gmail.com>
2023-09-09 16:42:19 -0400

    update for aws

--- database/database.js
+++ database/database.js
@@ -5,7 +5,12 @@ const urlParts = process.env.PG_CONNECTION_STRING.split(':');
 const dialect = urlParts[0].replace(/.*(?=postgres)/, 'postgres');
 const host = urlParts[2].replace(/\/\/|\/.*/g, '');
 
-module.exports = new Sequelize(process.env.PG_CONNECTION_STRING, {
+module.exports = new Sequelize(process.env.PG_CONNECTION_STRING,
+{
   dialect,
   host,
-});
+  dialectOptions: {
+    ssl: {rejectUnauthorized: false}
+  }
+}
+);
